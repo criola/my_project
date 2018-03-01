78eb1275e8c0660c4a5e69a2562e19ae2bcdb7ba
criola <cristian.olayaarias@wsu.edu>
2018-03-01 11:25:18 -0800

    add var names

--- analysis2.py
+++ analysis2.py
@@ -7,6 +7,10 @@ This is a temporary script file.
 
 import pandas as pd
 
-#working on oceania data
+    #working on oceania data
 
 europe = pd.read_csv('data/gapminder_gdp_europe.csv')
+
+oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
+
+africa = pd.read_csv('data/gapminder_gdp_africa.csv')
