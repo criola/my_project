612be415f84fa9fba87a36a8dc324442289ca2b6
criola <36965209+criola@users.noreply.github.com>
2018-03-01 11:11:52 -0800

    change var name

--- analysis.py
+++ analysis.py
@@ -7,4 +7,4 @@ This is a temporary script file.
 
 import pandas as pd
 
-dat = pd.read_csv('data/gapminder_gdp_oceania.csv', indez_col = 'country')
+oceania = pd.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country')
