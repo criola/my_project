9db8bfd7346bf1140d473b81dda2869d4da17955
criola <cristian.olayaarias@wsu.edu>
2018-03-01 11:28:38 -0800

    fix merge conflict

--- analysis2.py
+++ analysis2.py
@@ -9,8 +9,8 @@ import pandas as pd
 
     #working on oceania data
 
-europe = pd.read_csv('data/gapminder_gdp_europe.csv')
+europe = pd.read_csv('data/gapminder_gdp_europe.csv', index_col = 'country')
 
-oceania = pd.read_csv('data/gapminder_gdp_oceania.csv')
+oceania = pd.read_csv('data/gapminder_gdp_oceania.csv', index_col = 'country')
 
-africa = pd.read_csv('data/gapminder_gdp_africa.csv')
+africa = pd.read_csv('data/gapminder_gdp_africa.csv', index_col = 'country')
